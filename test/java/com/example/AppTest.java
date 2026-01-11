64b4d88967a983b2534101556b94d6671cf9112e
akshay <dhoke.akshay04@gmail.com>
2026-01-11 13:11:39 +0530

    My test java code

---
+++ test/java/com/example/AppTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.asserTrue;
+
+
+public class AppTest {
+
+@Test
+public void testApp() {
+ 
+	assertTrue(true);
+
+  }
+
+
+}
